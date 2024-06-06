fbf5e43d7eb897ca934258e14d0d1d0cf3f3c5cb
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-06-06 12:43:57 -0300

    feat: add new \deposit route

--- my-money-app/routes/web.php
+++ my-money-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,14 +11,10 @@ use App\Http\Controllers\ClientController;
 */
 
 
-// Route::post('/create-client', [ClientController::class, 'createNewClient']);
 $router = app('router');
 $router->post('/create-client',              "App\Http\Controllers\ClientController@createNewClient");
-// $router->get('/create-client',              "App\Http\Controllers\ClientController@createNewClient");
+$router->patch('/deposit',                   "App\Http\Controllers\TransactionController@deposit");
 
-// Route::post('/test-route', function() {
-//     return response()->json(['message' => 'Rota de teste funcionando']);
-// });
-// Route::get('/test-route', function() {
-//     return response()->json(['message' => 'Rota de teste funcionando']);
-// });
+
+// Route::post('/create-client', [ClientController::class, 'createNewClient']);
+// Route::patch('/deposit', [TransactionController::class, 'deposit']); 
